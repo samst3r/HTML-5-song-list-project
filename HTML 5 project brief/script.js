e88fea2782bcb80f0bc77f0468e45c68018b420f
samst3r <samuelshahzaman@gmail.com>
2026-01-14 12:19:33 -0500

    Add files via upload

--- HTML 5 project brief/script.js
+++ HTML 5 project brief/script.js
@@ -1,5 +1,3 @@
-const { createElement } = require("react");
-
 var xml = new XMLHttpRequest();
 
 xml.onreadystatechange = function () {
@@ -8,9 +6,23 @@ xml.onreadystatechange = function () {
     console.log(list);
     list.forEach((song) => {
       console.log(song);
-      let card = documdent.createElement("div");
-      card.classList.add("songCard");
-      let textData = "";
+      let card = document.createElement("div");
+      card.id = "songCard";
+
+      let textData =
+        "<img id='img' src='" +
+        song.songImg +
+        "' /><div id='songInfo'><p id='songName'>" +
+        song.name +
+        "</p><div id='songDetails'><p id='artistName'>Artist: " +
+        song.artist +
+        "</p><p id='genre'>Genre: " +
+        song.genre +
+        "</p><p id='duration'>Duration: 6:03</p></div><svgid='play'xmlns='http://www.w3.org/2000/svg'width='2em'height='2em'viewBox='0 0 24 24'id='_24x24_On_Light_Play'data-name='24x24/On Light/Play'><rect id='view-box' width='24' height='24' fill='none' /><pathid='Shape'd='M0,9.75A9.75,9.75,0,1,1,9.75,19.5,9.761,9.761,0,0,1,0,9.75Zm1.5,0A8.25,8.25,0,1,0,9.75,1.5,8.259,8.259,0,0,0,1.5,9.75Zm5.75-3.8,7,3.8-7,3.8Z'transform='translate(2.25 2.25)'fill='#141124'/></svg></div>";
+      card.innerHTML = textData;
+      console.log(textData);
+      console.log(card.innerHTML);
+      document.querySelector("#songlist").appendChild(card);
     });
   }
 };
