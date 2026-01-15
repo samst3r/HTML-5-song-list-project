28aa990b20e38a0f4126a0ec14018e213624b08a
samst3r <samuelshahzaman@gmail.com>
2026-01-15 12:17:56 -0500

    did more formatting
    Correctly formatted time + created playlist to add songs

--- HTML 5 project brief/script.js
+++ HTML 5 project brief/script.js
@@ -10,10 +10,12 @@ xml.onreadystatechange = function () {
       card.id = "songCard";
       card.style.backgroundColor = song.cardColor;
       console.log(song.name + ":" + song.duration);
-      console.log(song.name + " seconds:" + (song.duration % 60));
-      console.log(
-        song.name + " minutes:" + (song.duration - (song.duration % 60)) / 60
-      );
+      let seconds = song.duration % 60;
+      seconds = String(seconds).padStart(2, "0");
+      console.log(song.name + " seconds:" + seconds);
+      let minutes = (song.duration - (song.duration % 60)) / 60;
+      console.log(song.name + " minutes:" + minutes);
+      //start of innerHTML data
       let textData =
         "<img id='img' src='" +
         song.songImg +
@@ -25,7 +27,11 @@ xml.onreadystatechange = function () {
         song.artist +
         "</p><p id='genre'>Genre: " +
         song.genre +
-        "</p><p id='duration'>Duration: 6:03</p></div><svgid='play'xmlns='http://www.w3.org/2000/svg'width='2em'height='2em'viewBox='0 0 24 24'id='_24x24_On_Light_Play'data-name='24x24/On Light/Play'><rect id='view-box' width='24' height='24' fill='none' /><pathid='Shape'd='M0,9.75A9.75,9.75,0,1,1,9.75,19.5,9.761,9.761,0,0,1,0,9.75Zm1.5,0A8.25,8.25,0,1,0,9.75,1.5,8.259,8.259,0,0,0,1.5,9.75Zm5.75-3.8,7,3.8-7,3.8Z'transform='translate(2.25 2.25)'fill='#141124'/></svg></div>";
+        "</p><p id='duration'>Duration: " +
+        minutes +
+        ":" +
+        seconds +
+        "</p></div><svgid='play'xmlns='http://www.w3.org/2000/svg'width='2em'height='2em'viewBox='0 0 24 24'id='_24x24_On_Light_Play'data-name='24x24/On Light/Play'><rect id='view-box' width='24' height='24' fill='none' /><pathid='Shape'd='M0,9.75A9.75,9.75,0,1,1,9.75,19.5,9.761,9.761,0,0,1,0,9.75Zm1.5,0A8.25,8.25,0,1,0,9.75,1.5,8.259,8.259,0,0,0,1.5,9.75Zm5.75-3.8,7,3.8-7,3.8Z'transform='translate(2.25 2.25)'fill='#141124'/></svg></div>";
       card.innerHTML = textData;
       console.log(textData);
       console.log(card.innerHTML);
