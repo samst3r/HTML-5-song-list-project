b110ee2ce4ef57c95a2fdcfc4c5c60e556fdd0ea
samst3r <samuelshahzaman@gmail.com>
2026-02-04 12:10:04 -0500

    added more user creativity with adding songs

--- HTML 5 project brief/form.js
+++ HTML 5 project brief/form.js
@@ -1,15 +1,17 @@
 let form = document.querySelector("#form");
-let songTitleInput=document.querySelector("#songName");
-let artistInput=document.querySelector("#artist");
-let durationInput=document.querySelector("#duration");
-let genreInput=document.querySelector("#genre");
-let imageInput=document.querySelector("#image");
+let songTitleInput = document.querySelector("#songName");
+let artistInput = document.querySelector("#artist");
+let durationInput = document.querySelector("#duration");
+let genreInput = document.querySelector("#genre");
+let imageInput = document.querySelector("#image");
+let cardColorInput = document.querySelector("#cardColor");
+let textColorInput = document.querySelector("#textColor");
 
 let returnButton = document.querySelector("#returnButton");
-returnButton.addEventListener("click", function(e) {
-e.preventDefault();
-  window.location.replace("index.html");
-  console.log("clicked")
+returnButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.assign("index.html");
+  console.log("clicked");
 });
 
 if (localStorage.getItem("songData")) {
@@ -19,22 +21,23 @@ if (localStorage.getItem("songData")) {
 }
 
 form.addEventListener("submit", function (e) {
-  let image="images/meowl.jpeg";
+  let image = "images/meowl.jpeg";
   e.preventDefault();
-  if(imageInput.value!=""){
-image=imageInput.value;
+  if (imageInput.value != "") {
+    image = imageInput.value;
   }
   let newSong = {
-"name": songTitleInput.value,
-"artist": artistInput.value,
-"duration": durationInput.value,
-"genre": genreInput.value,
-"songImg": image,
-"songFile": "",
-"cardColor": "#FFFFFF",
-"textColor": "#000000"
+    name: songTitleInput.value,
+    artist: artistInput.value,
+    duration: durationInput.value,
+    genre: genreInput.value,
+    songImg: image,
+    songFile: "",
+    cardColor: cardColorInput.value,
+    textColor: textColorInput.value,
   };
-data.push(newSong);
-localStorage.setItem("songData",JSON.stringify(data))
+  data.push(newSong);
+  localStorage.setItem("songData", JSON.stringify(data));
   form.reset();
+  window.location.assign("index.html");
 });
